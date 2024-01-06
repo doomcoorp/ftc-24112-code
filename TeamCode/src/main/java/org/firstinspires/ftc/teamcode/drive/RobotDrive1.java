eb5f5cccbc750537ff2f0013b43e19d363697ff7
doomcoorp <149755897+doomcoorp@users.noreply.github.com>
2024-01-05 16:22:45 -0800

    qd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RobotDrive1.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+
+import com.acmerobotics.roadrunner.drive.Drive;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+
+
+@TeleOp
+public class RobotDrive1 extends LinearOpMode {
+    protected DcMotor left_arm;
+    protected DcMotor right_arm;
+    protected Servo arm2_servo;
+    protected Servo hand_servo;
+    protected Servo drone_servo;
+    protected ElapsedTime runtime = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(0, 0, 0);
+
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+                .splineTo(new Vector2d(10, 10), 0)
+                .turn(Math.toRadians(90))
+                .splineTo(new Vector2d(25, -15), 0)
+                .waitSeconds(3)
+                .turn(Math.toRadians(45))
+                .forward(10)
+                .strafeRight(5)
+                .turn(Math.toRadians(90))
+                .strafeLeft(5)
+                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(-10, -10, Math.toRadians(45)), 0)
+                .build();
+
+        waitForStart();
+
+        if (!isStopRequested())
+            drive.followTrajectorySequence(trajSeq);
+    }
+}
